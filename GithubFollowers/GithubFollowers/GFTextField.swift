95af4b43bb08adcf062bb0149fa6aec28d697bb0
Hoon94 <dleognsdl1@naver.com>
2024-03-22 01:51:19 +0900

    feat: Add reusable textfield using the GF prefix, whitch means Github Followers

---
+++ GithubFollowers/GithubFollowers/GFTextField.swift
@@ -0,0 +1,37 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by Daehoon Lee on 2024/03/22.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = .preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        backgroundColor = .tertiarySystemBackground
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
