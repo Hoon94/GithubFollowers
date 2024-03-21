a01fb9b8db6d4c799e948a9b4586ab28fdbab0cf
Hoon94 <dleognsdl1@naver.com>
2024-03-22 01:33:18 +0900

    feat: Add reusable button using the GF prefix, whitch means Github Followers

---
+++ GithubFollowers/GithubFollowers/GFButton.swift
@@ -0,0 +1,33 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Daehoon Lee on 2024/03/22.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
