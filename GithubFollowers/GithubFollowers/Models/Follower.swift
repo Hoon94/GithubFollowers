84c45e39276f19d3055365b1f874cf532e2d39e4
Hoon94 <dleognsdl1@naver.com>
2024-03-24 01:42:49 +0900

    feat: Create Follower model

---
+++ GithubFollowers/GithubFollowers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GithubFollowers
+//
+//  Created by Daehoon Lee on 2024/03/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
