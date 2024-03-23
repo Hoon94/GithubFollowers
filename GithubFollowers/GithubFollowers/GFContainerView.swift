b85db7506b0210fdc2dbfdd950d82e1df9261762
Hoon94 <dleognsdl1@naver.com>
2024-03-24 00:35:21 +0900

    feat: Add reusable container view using the GF prefix, whitch means Github Followers

---
+++ GithubFollowers/GithubFollowers/GFContainerView.swift
@@ -0,0 +1,27 @@
+//
+//  GFContainerView.swift
+//  GithubFollowers
+//
+//  Created by Daehoon Lee on 2024/03/24.
+//
+
+import UIKit
+
+class GFContainerView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
