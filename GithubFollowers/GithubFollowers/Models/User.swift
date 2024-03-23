2bb53d3a6baf232640b3f4fc041a3750e39af36d
Hoon94 <dleognsdl1@naver.com>
2024-03-24 01:43:27 +0900

    feat: Create User model

---
+++ GithubFollowers/GithubFollowers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  GithubFollowers
+//
+//  Created by Daehoon Lee on 2024/03/24.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
