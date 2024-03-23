d8739c7eb37a57f08d18a414f96a41f98dd439a6
Hoon94 <dleognsdl1@naver.com>
2024-03-23 18:22:53 +0900

    feat: Add UIViewController extension for presenting alert

---
+++ GithubFollowers/GithubFollowers/UIViewController+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Ext.swift
+//  GithubFollowers
+//
+//  Created by Daehoon Lee on 2024/03/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertViewController = GFAlertViewController(title: title, message: message, buttonTitle: buttonTitle)
+            alertViewController.modalPresentationStyle = .overFullScreen
+            alertViewController.modalTransitionStyle = .crossDissolve
+            self.present(alertViewController, animated: true)
+        }
+    }
+}
