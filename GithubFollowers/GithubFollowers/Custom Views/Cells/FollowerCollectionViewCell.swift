d57a49f6a714a478ff67e3de1baa7de6096fddc3
Hoon94 <dleognsdl1@naver.com>
2024-03-24 23:57:24 +0900

    feat: Add reusable avatar image view using the GF prefix, whitch means Github Followers

--- GithubFollowers/GithubFollowers/Custom Views/Cells/FollowerCollectionViewCell.swift
+++ GithubFollowers/GithubFollowers/Custom Views/Cells/FollowerCollectionViewCell.swift
@@ -10,6 +10,6 @@ import UIKit
 class FollowerCollectionViewCell: UICollectionViewCell {
     static let reuseID = "FollowerCell"
     
-    let avatarImageView = UIImageView()
+    let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
 }
