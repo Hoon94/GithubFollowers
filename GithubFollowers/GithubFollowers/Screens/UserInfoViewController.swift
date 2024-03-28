7906f2e7ea29a47ba417cc7b4b85147e5c981ef3
Hoon94 <dleognsdl1@naver.com>
2024-03-28 17:24:28 +0900

    refactor: Modify date decoding strategy

--- GithubFollowers/GithubFollowers/Screens/UserInfoViewController.swift
+++ GithubFollowers/GithubFollowers/Screens/UserInfoViewController.swift
@@ -57,7 +57,7 @@ class UserInfoViewController: UIViewController {
         self.add(childViewController: GFUserInfoHeaderViewController(user: user), to: self.headerView)
         self.add(childViewController: repoItemViewController, to: self.itemViewOne)
         self.add(childViewController: followerItemViewController, to: self.itemViewTwo)
-        self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
+        self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())"
     }
     
     private func layoutUI() {
