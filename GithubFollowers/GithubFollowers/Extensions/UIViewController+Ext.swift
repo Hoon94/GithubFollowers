771fc8ccfc94f808f1edffa9da8f23c7b59a3bb6
Hoon94 <dleognsdl1@naver.com>
2024-03-28 18:50:16 +0900

    feat: Add reusable data loading controller using the GF prefix, whitch means Github Followers

--- GithubFollowers/GithubFollowers/Extensions/UIViewController+Ext.swift
+++ GithubFollowers/GithubFollowers/Extensions/UIViewController+Ext.swift
@@ -5,10 +5,8 @@
 //  Created by Daehoon Lee on 2024/03/23.
 //
 
-import UIKit
+import Foundation
 import SafariServices
-
-fileprivate var containerView: UIView!
 
 extension UIViewController {
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
@@ -24,41 +22,5 @@ extension UIViewController {
         let safariViewController = SFSafariViewController(url: url)
         safariViewController.preferredControlTintColor = .systemGreen
         present(safariViewController, animated: true)
-    }
-    
-    func showLoadingView() {
-        containerView = UIView(frame: view.bounds)
-        view.addSubview(containerView)
-        
-        containerView.backgroundColor = .systemBackground
-        containerView.alpha = 0
-        
-        UIView.animate(withDuration: 0.25) {
-            containerView.alpha = 0.8
-        }
-        
-        let activityIndicator = UIActivityIndicatorView(style: .large)
-        containerView.addSubview(activityIndicator)
-        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-        
-        activityIndicator.startAnimating()
-    }
-    
-    func dismissLoadingView() {
-        DispatchQueue.main.async {
-            containerView.removeFromSuperview()
-            containerView = nil
-        }
-    }
-    
-    func showEmptyStateView(with message: String, in view: UIView) {
-        let emptyStateView = GFEmptyStateView(message: message)
-        emptyStateView.frame = view.bounds
-        view.addSubview(emptyStateView)
     }
 }
